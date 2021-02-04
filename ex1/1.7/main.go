d06cce56beb5d8449db05ce727e5a7cb1709ea8b
Doug <doug@anetsc.com>
2021-02-03 16:38:47 -0800

    Exercises 1.7 - 1.9 Page 17

---
+++ ex1/1.7/main.go
@@ -0,0 +1,35 @@
+/*
+Exercise 1.7  p. 17
+The function call io.Copy(dst, src) reads from src and writes to dst .
+Use it instead of ioutil.ReadAll to copy the response body to os.Stdout
+without requiring a buffer large enough to hold the entire stream.
+Be sure to check the error result of io.Copy .
+*/
+
+// Fetch prints the content found at each specified URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+		fmt.Printf("************ copy url: %s ********************** \n", url)
+		bytes, err := io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("\n************ end url: %s, wrote %d bytes ********************** \n", url, bytes)
+	}
+}
---
+++ ex1/1.9/main.go
@@ -0,0 +1,37 @@
+/*
+Exercise 1.9: Modify fetch to also print the HTTP status code, found in resp.Status .
+*/
+
+// Fetch prints the content found at each specified URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		// ensure URL begins with http://
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+		fmt.Printf("************ copy url: %s ********************** \n", url)
+		bytes, err := io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("\n************ end url: %s. Read %d bytes. Status code: %s ********************** \n", url, bytes, resp.Status)
+	}
+}
